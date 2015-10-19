24adcf66115d167052b1f4b4478e53677d71c4ef
maruware <me@maruware.com>
2015-10-20 01:08:36 +0900

    Minimum works

---
+++ api.rb
@@ -0,0 +1,47 @@
+require 'grape'
+
+class API < Grape::API
+  # version 'v1', using: :header
+  format :json
+
+  helpers do
+    def articles
+      [
+        {
+          id: 1,
+          title: 'good',
+          text: 'good content',
+        },
+        {
+          id: 2,
+          title: 'bad',
+          text: 'bad content',
+        },
+      ]
+    end
+  end
+
+  resource :articles do
+    #/articles
+    get do
+      articles
+    end
+
+    #/articles/1
+    route_param :id do
+      get do
+        articles.find{|a| a[:id] == params['id'].to_i}
+      end
+
+      post do
+        {
+          id: 2,
+          title: params['title'],
+          text: params['text'],
+        }
+      end
+    end
+
+  end
+
+end
